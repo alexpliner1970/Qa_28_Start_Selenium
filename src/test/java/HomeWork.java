7eabc4ece26400529f61e4d9f3865cf14940afe0
alexpliner1970 <alexpliner3005@gmail.com>
2025-02-01 18:23:06 +0200

    HomeWork_01_02_2025

--- src/test/java/HomeWork.java
+++ src/test/java/HomeWork.java
@@ -33,5 +33,46 @@ public class HomeWork {
 
         List<WebElement> button =drv.findElements(By.tagName("button"));
         List<WebElement> button1 =drv.findElements(By.cssSelector("button"));
+
+
+         //===HomeWork_01_02_2025
+
+        //by class
+
+        WebElement div2 =drv.findElement(By.className("container"));
+        WebElement div3 =drv.findElement(By.cssSelector(".container"));
+
+        WebElement div4 =drv.findElement(By.className("navbar-component_nav__1X_4m"));
+        WebElement div5= drv.findElement(By.cssSelector(".navbar-component_nav__1X_4m"));
+
+        WebElement div6=drv.findElement(By.className("login_login__3EHKB"));
+        WebElement div7= drv.findElement(By.cssSelector(".login_login__3EHKB"));
+
+        WebElement a2 = drv.findElement(By.className("active"));
+        WebElement a3 = drv.findElement(By.cssSelector(".active"));
+
+        //by id
+
+        WebElement div8 =drv.findElement(By.id("root"));
+        WebElement div9 =drv.findElement(By.cssSelector("#root"));
+
+        //by attribute
+
+        WebElement input2 = drv.findElement(By.cssSelector("input[placeholder='Email']"));
+        WebElement input3 = drv.findElement(By.cssSelector("[placeholder^='Pas']")); //Password
+
+        WebElement button2 =drv.findElement(By.cssSelector("[name$='gin']")); //login
+        WebElement button3 =drv.findElement(By.cssSelector("[name*='istr']")); //registration
+
+        //xpath
+
+        WebElement a4 =drv.findElement(By.xpath("//a[@href='/home']")); // "/home"
+        WebElement a5 =drv.findElement(By.xpath("//*[starts-with(@href,'/ab')]")); // "/about"
+        WebElement a6 =drv.findElement(By.xpath("//*[contains(@href,'og')]")); // "/login"
+
+        WebElement h1_2 =drv.findElement(By.xpath("//*[text()='PHONEBOOK']"));
+
+
+
     }
 }
--- src/test/java/Index.java
+++ src/test/java/Index.java
@@ -24,6 +24,7 @@ public class Index {
     public void tableTest(){
         //Canada
         WebElement canada = wd.findElement(By.cssSelector("tr:nth-child(3) td:last-child"));
+       // WebElement canada = wd.findElement(By.linkText("Canada"));
         Assert.assertEquals(canada.getText(),"Canada");
 
     }
